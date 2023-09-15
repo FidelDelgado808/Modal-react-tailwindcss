1d118614fe4c7481f30c28585b02e81fb87195bc
FidelDelgado808 <79215339+FidelDelgado808@users.noreply.github.com>
2023-09-15 15:58:03 +0200

    Added and updated Flex.js - template for modal

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -30,10 +30,56 @@ export default function Flex({ handleSubmitData }) {
 	};
 	const localFormData = getLocalStorageFormData();
 
+	return (
+		<div className="flex space-x-4 flex-row">
+			<div>
+				<img
+					src="https://s2.qwant.com/thumbr/700x0/f/e/ce190e7d24f10478eb6f9d867034934af36688d3f29314fb58baa27ec3339e/Facebook_-How-To-Save-Money_-100-Tips-To-Save-Money-Fast.png?u=https%3A%2F%2Fwww.thewaystowealth.com%2Fwp-content%2Fuploads%2F2017%2F03%2FFacebook_-How-To-Save-Money_-100-Tips-To-Save-Money-Fast.png&q=0&b=1&p=0&a=0"
+					className=" w-80
+						rounded-md"
+					alt="..."
+				/>
+			</div>
 
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className='py-2 mb-2'>
 
-  return (
+				<input
+					defaultValue={localFormData?.navn}
+					className="border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
+					placeholder="Navn"
+					{...register('navn')}
+					/>
+				<div>
+					<input
+						defaultValue={localFormData?.email}
+						className="border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
+						{...register('email', { required: true })}
+						placeholder="E-mail"
+						type="email"
+						/>
 
-<div className='flex'></div>
+					{errors.email && (
+						<span className="flex py-2 px-4">
+							Du mangler at udfylde et felt.
+						</span>
+					)}
 
-  )
+					</div>
+					{/* Button */}
+				</div>
+				<div className="mb-5">
+					<input
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded 
+				focus:outline-none focus:shadow-outline"
+						type="submit"
+						autoComplete="on"
+						value={'Submit'}
+					/>
+				</div>
+			</form>
+		</div>
+	);
+}
+// to-do:
+// Husk at gøre denne responsiv

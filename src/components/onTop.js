cde6c08c3ca7c6c1de33cd52681a56ffb639d016
FidelDelgado808 <79215339+FidelDelgado808@users.noreply.github.com>
2023-10-13 11:59:26 +0200

    left right center works now
    ontop is not done

--- src/components/onTop.js
+++ src/components/onTop.js
@@ -3,7 +3,13 @@ import { useForm } from 'react-hook-form';
 import Confetti from 'react-confetti';
 import Success from './Success';
 
-function App({ closeModal, headingOne, headingTwo, positionStyle, paragraf }) {
+function Modal({
+	closeModal,
+	headingOne,
+	headingTwo,
+	positionStyle,
+	paragraf,
+}) {
 	const [showSuccess, setShowSuccess] = useState(false);
 
 	const [userData, setUserData] = useState({
@@ -27,9 +33,7 @@ function App({ closeModal, headingOne, headingTwo, positionStyle, paragraf }) {
 		img1: 'https://tecdn.b-cdn.net/img/new/slides/041.jpg',
 		img2: 'https://media.istockphoto.com/id/960567928/photo/happy-man-drinking-a-cup-of-coffee-at-a-cafe.jpg?s=2048x2048&w=is&k=20&c=bHRrhIWp3Na9GjnNZZk58PbLzJXluXWzlUyNSKdq7tY=',
 		img3: 'https://s2.qwant.com/thumbr/700x0/f/e/ce190e7d24f10478eb6f9d867034934af36688d3f29314fb58baa27ec3339e/Facebook_-How-To-Save-Money_-100-Tips-To-Save-Money-Fast.png?u=https%3A%2F%2Fwww.thewaystowealth.com%2Fwp-content%2Fuploads%2F2017%2F03%2FFacebook_-How-To-Save-Money_-100-Tips-To-Save-Money-Fast.png&q=0&b=1&p=0&a=0',
-		img4: 'https://s2.qwant.com/thumbr/0x380/5/0/09e2ed10b1d5797bb796da4c38262e5a06e9d129cb4c140eeff1c4fff52f59/Vertical-Horizon-104.jpg?u=https%3A%2F%2Fpetapixel.com%2Fassets%2Fuploads%2F2017%2F04%2FVertical-Horizon-104.jpg&q=0&b=1&p=0&a=0',
-		img5: 'https://s1.qwant.com/thumbr/0x380/9/c/30fe2233b14c04045e4b8ee28c01c1edb43223c484775833c152c74d5fb5b9/529571.jpg?u=https%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2Fe%2Fe%2F3%2F529571.jpg&q=0&b=1&p=0&a=0',
-		img6: '',
+		img4: '',
 	});
 
 	function handleClick() {
@@ -85,10 +89,11 @@ function App({ closeModal, headingOne, headingTwo, positionStyle, paragraf }) {
 
 	const localFormData = getLocalStorageFormData();
 
-	const [imgPosition] = useState({
-		center: 'rounded-lg',
-		right: 'h-full rounded-lg',
-		left: 'h-full rounded-md',
+	const [imgPosition, setImgPosition] = useState({
+		center: 'flex-row, rounded-lg',
+		left: 'flex-col, rounded-lg',
+		right: 'top-11 left-16 rounded-lg',
+		onTop: 'flex-wrap',
 	});
 
 	return (
@@ -97,8 +102,8 @@ function App({ closeModal, headingOne, headingTwo, positionStyle, paragraf }) {
 			className="absolute inset-0 w-screen h-screen bg-zinc-700/50 flex justify-center items-center"
 			onClick={closeModalBgClick}
 		>
-			<div className="bg-white rounded-lg md:w-7/12 w-10/12 max-w-screen-md shadow-xl relative">
-				<a onClick={closeModal} className="absolute right-4 top-1 text-xl">
+			<div className="bg-white p-4 m-4 rounded-lg md:w-7/12 w-10/12 max-w-screen-md shadow-2xl relative">
+				<a onClick={closeModal} className="absolute right-5 text-xl">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -115,85 +120,99 @@ function App({ closeModal, headingOne, headingTwo, positionStyle, paragraf }) {
 					</svg>
 				</a>
 
-				{!showSuccess && (
-					<div
-						className={`flex pt-8
-								${positionStyle === 'center' ? 'flex-col-reverse' : ''}
-								${positionStyle === 'left' ? 'flex-row-reverse' : ''}
-								${positionStyle === 'right' ? 'flex-row' : ''}
-						}`}
-					>
-						<div className="min-w-2/3">
-							<div className="text-4xl py-4 font-bold capitalize">
-								{headingOne}
-							</div>
-							<div className="px-8 text-lg">{paragraf}</div>
-
-							<form onSubmit={handleSubmit(onSubmit)}>
-								<div className="py-2 mb-2">
-									<input
-										defaultValue={localFormData?.navn}
-										className="border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
-										placeholder="Navn"
-										{...register('navn')}
-									/>
-									{/* Inputs */}
-									<div>
-										<input
-											defaultValue={localFormData?.email}
-											className="border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
-											{...register('email', { required: true })}
-											placeholder="E-mail"
-											type="email"
-										/>
-
-										{errors.email && (
-											<span className="py-2 px-4 text-sm">
-												Du mangler at udfylde et felt.
-											</span>
-										)}
-									</div>
-									<div>
-										<input
-											type="checkbox"
-											className="required:border-red-500 mb-2"
-											required
-										/>
-										<span className="text-sm">
-											&nbsp; I want awesome newsletters with occasional
-											discounts
-										</span>
-									</div>
-
-									{/* Button */}
-									<input
-										className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded 
-									focus:outline-none focus:shadow-outline"
-										type="submit"
-										autoComplete="on"
-										value={'Submit'}
-									/>
-								</div>
-							</form>
-						</div>
-
-						<div className="h-full w-full max-w-1/4">
+				<div
+					className={`flex ${
+						positionStyle === 'center' ? 'flex-col' : 'flex-row'
+					}`}
+				>
+					{!showSuccess && (
+						<div className="relative right-0">
 							<img
-								src={ImageSource.img5}
+								src={`${ImageSource.img2}`}
 								className={imgPosition[positionStyle]}
 								alt="img"
 							/>
 						</div>
-					</div>
+					)}
+
+					<div className="text-4xl"></div>
+
+					{!showSuccess && (
+						<div className="flex-1 text-4xl py-2 font-bold capitalize">
+							{headingOne}
+						</div>
+					)}
+					{!showSuccess && (
+						<div className="bg-blue-500 w-6/12 h-1 mx-auto rounded-lg"></div>
+					)}
+
+					{!showSuccess && (
+						<div className="flex-2 text-xl py-2">{headingTwo}</div>
+					)}
+					{!showSuccess && (
+						<div className="flex-2 text-lg py-2">{paragraf}</div>
+					)}
+				</div>
+
+				{!showSuccess && (
+					<>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div className="py-2 mb-2">
+								<input
+									defaultValue={localFormData?.navn}
+									className="flex-3 border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
+									placeholder="Navn"
+									{...register('navn')}
+								/>
+								{/* Inputs */}
+								<div>
+									<input
+										defaultValue={localFormData?.email}
+										className="flex-4 border rounded w-full py-2 px-3 my-2 text-gray-700 leading-tight"
+										{...register('email', { required: true })}
+										placeholder="E-mail"
+										type="email"
+									/>
+
+									{errors.email && (
+										<span className="flex-5 py-2 px-4 justify-center">
+											Du mangler at udfylde et felt.
+										</span>
+									)}
+								</div>
+								<div>
+									<input
+										type="checkbox"
+										className="required:border-red-500 mb-2"
+										required
+									/>
+									<span className="text-sm">
+										&nbsp; I want awesome to receive newsletters with occasional
+										discounts
+									</span>
+								</div>
+
+								{/* Button */}
+								<input
+									className="flex-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded 
+									focus:outline-none focus:shadow-outline"
+									type="submit"
+									autoComplete="on"
+									value={'Submit'}
+								/>
+							</div>
+						</form>
+					</>
 				)}
 
 				{showSuccess && userData && (
 					<Success userData={userData} closeModal={closeModal} />
 				)}
 			</div>
-			{showSuccess && <Confetti numberOfPieces={pieces} gravity={0.2} />}
+
+			{showSuccess && <Confetti numberOfPieces={pieces} gravity={0.7} />}
 		</div>
 	);
 }
 
-export default App;
+export default Modal;
